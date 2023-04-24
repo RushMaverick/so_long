d6da8c6da2d2142b9127cafd3527081643e2de78
Rasmus Rask <rasse.rask@gmail.com>
2023-04-24 12:35:15 +0300

    Detecting when map is not rectangular, next up is working to get it to exit safely.

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: rrask <rrask@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 17:31:35 by rrask             #+#    #+#             */
-/*   Updated: 2023/04/20 17:05:35 by rrask            ###   ########.fr       */
+/*   Updated: 2023/04/24 12:30:48 by rrask            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,41 +16,48 @@
 # define MOVE_Y 0
 # define WIN_WIDTH 640
 # define WIN_HEIGHT 576
-# define FRONT	"src/sprites/front.xpm"
-# define BACK	"src/sprites/back.xpm"
-# define LEFT	"src/sprites/left.xpm"
-# define RIGHT	"src/sprites/right.xpm"
-# include "mlx.h"
+# define FRONT "src/sprites/front.xpm"
+# define BACK "src/sprites/back.xpm"
+# define LEFT "src/sprites/left.xpm"
+# define RIGHT "src/sprites/right.xpm"
+# define WALL "src/sprites/wall.xpm"
+# define EXIT "src/sprites/exit.xpm"
+# define EMPTY "src/sprites/empty.xpm"
 # include "libft.h"
+# include "mlx.h"
 
-typedef enum e_game_obj
+// typedef enum e_game_obj
+// {
+// 	EMPTY,
+// 	PLAYER,
+// 	WALL,
+// 	EXIT,
+// 	COLLECTIBLE,
+// }				t_game_obj;
+
+typedef struct s_vars
 {
-	EMPTY,
-	PLAYER,
-	WALL,
-	EXIT,
-	COLLECTIBLE,
-}				t_game_obj;
-
-typedef struct s_vars{
-	void	*mlx;
-	void	*win;
-	void	*img;
-	int		place;
+	void		*mlx;
+	void		*win;
+	void		*img;
+	int			place;
 }				t_vars;
 
-typedef struct s_game{
-	int		resx;
-	int		resy;
+typedef struct s_game
+{
+	int			resx;
+	int			resy;
 }				t_game;
 
-typedef struct s_map{
-	int		width;
-	int		height;
-	t_game_obj	**map;
+typedef struct s_map
+{
+	int			width;
+	int			height;
+	char		**map;
 }				t_map;
 
-typedef enum e_keyevent{
+typedef enum e_keyevent
+{
 	KEY_A = 0,
 	KEY_S = 1,
 	KEY_D = 2,
@@ -65,12 +72,13 @@ typedef enum e_keyevent{
 	ESC = 53
 }				t_keyevent;
 
-
-int		key_handler(int keycode, t_vars *vars);
-int		image_handler(t_vars *vars, t_game *game);
-void	init(t_game *game, t_vars *vars);
-void	map_checker(char *line);
-void	map_reader(int fd, t_map *map);
-void	map_validator(t_map *map);
+int				key_handler(int keycode, t_vars *vars);
+int				image_handler(t_vars *vars, t_game *game);
+void			init(t_game *game, t_vars *vars);
+void			map_checker(char *line);
+void			map_reader(int fd, t_map *map);
+void			map_validator(t_map *map, char **whole_map);
+void			map_placement(char c);
+void			is_map_rectangular(t_map *map);
 
 #endif
--- src/map_utils.c
+++ src/map_utils.c
@@ -6,23 +6,21 @@
 /*   By: rrask <rrask@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 15:50:28 by rrask             #+#    #+#             */
-/*   Updated: 2023/04/20 17:38:06 by rrask            ###   ########.fr       */
+/*   Updated: 2023/04/24 12:33:59 by rrask            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-#include <stdio.h>
-
 void	map_reader(int fd, t_map *map)
 {
 	char	*str;
 	char	*map_array;
-	char	**map;
+	char	**whole_map;
 
 	map->height = 0;
 	map_array = malloc(1);
-	while (42)
+	while (1) //LEAK IN HERE SOMEWHERE
 	{
 		str = get_next_line(fd);
 		if (str == NULL)
@@ -31,29 +29,45 @@ void	map_reader(int fd, t_map *map)
 		free(str);
 		map->height++;
 	}
-	map = ft_split(map_array, '\n');
+	whole_map = ft_split(map_array, '\n');
 	free(map_array);
-	map_validator(map);
+	free(str);
+	map_validator(map, whole_map);
 }
 
-void	map_validator(char **map)
+void	map_validator(t_map *map, char **whole_map)
 {
 	int	i;
 	int	j;
+	int	comp_width;
 
 	i = 0;
 	j = 0;
-	while (map[i] != '\0')
+	comp_width = ft_strlen(*whole_map);
+	while (whole_map[i] != '\0')
 	{
 		j = 0;
-		ft_printf("%s", map[i]);
-		while (map[i][j] != '\0')
+		map->width = ft_strlen(whole_map[i]);
+		if (map->width != comp_width)
 		{
-			if (map[i][j] != '\n')
+			free(whole_map);
+			ft_printf("%s\n", "That's wrong");
+			exit (1);
+		}
+		while (whole_map[i][j] != '\0')
+		{
+			map_placement(whole_map[i][j]);
+			if (whole_map[i][j] != '\n')
 				j++;
 		}
 		i++;
+		ft_printf("\n");
 	}
+}
+
+void	map_placement(char c)
+{
+	ft_printf("%c", c);
 }
 
 void	map_checker(char *file_name)
@@ -64,5 +78,5 @@ void	map_checker(char *file_name)
 	fd = open(file_name, O_RDONLY);
 	map_reader(fd, &map);
 	//free_map
-	// ft_printf("%d tiles, with a number of %d columns.\n", map.width, map.col_count);
+	//ft_printf("%d tiles, with a number of %d columns.\n", map.width, map.col_count);
 }
